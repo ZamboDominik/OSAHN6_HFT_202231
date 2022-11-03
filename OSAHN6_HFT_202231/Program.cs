db7a4d4378e86cde56fbc1ff6f862fbd0c18e0db
Domi <Domi@DESKTOP-4EVEHKC>
2022-11-03 20:50:05 +0100

    Add project files.

---
+++ OSAHN6_HFT_202231/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OSAHN6_HFT_202231
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
