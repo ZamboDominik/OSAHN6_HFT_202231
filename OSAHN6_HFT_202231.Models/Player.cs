7f3553e3863743d7d35cbdfb8d8d2fb359fce2cd
Dominik Zámbó <zambo.dominik@outlook.com>
2022-11-11 16:32:13 +0100

    Class creation

---
+++ OSAHN6_HFT_202231.Models/Player.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSAHN6_HFT_202231.Models
+{
+    internal class Player
+    {
+    }
+}
