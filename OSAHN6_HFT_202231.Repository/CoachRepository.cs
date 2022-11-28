b05bc0668acb21430cf11c5e0ca0c4126c8fdc82
Dominik Zámbó <zambo.dominik@outlook.com>
2022-11-28 09:19:12 +0100

    Repository final

---
+++ OSAHN6_HFT_202231.Repository/CoachRepository.cs
@@ -0,0 +1,31 @@
+﻿using OSAHN6_HFT_202231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSAHN6_HFT_202231.Repository
+{
+    public class CoachRepository:Repository<Coach>
+    {
+        public CoachRepository(NBADbContext ctx) : base(ctx)
+        {
+        }
+
+        public override Coach Read(int id)
+        {
+            return ctx.Coaches.FirstOrDefault(b => b.CoachId == id);
+        }
+
+        public override void Update(Coach item)
+        {
+            var old = Read(item.CoachId);
+            foreach (var prop in item.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
---
+++ OSAHN6_HFT_202231.Repository/PlayerRepository.cs
@@ -0,0 +1,31 @@
+﻿using OSAHN6_HFT_202231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSAHN6_HFT_202231.Repository
+{
+    public class PlayerRepository:Repository<Player>
+    {
+        public PlayerRepository(NBADbContext ctx) : base(ctx)
+        {
+        }
+
+        public override Player Read(int id)
+        {
+            return ctx.Players.FirstOrDefault(b => b.PlayerId == id);
+        }
+
+        public override void Update(Player item)
+        {
+            var old = Read(item.PlayerId);
+            foreach (var prop in item.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
